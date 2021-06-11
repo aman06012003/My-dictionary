cab910b19c8118f44da888274b8434c04f862f78
Aman Kapoor <akalasklo@gmail.com>
2021-06-12 01:21:33 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,7 @@
+print('Welcome to my dictionary')
+
+dic1 = {'abacus':'A frame of wire with rowsof wires along which beads are slid', 'abandon': 'give up'}
+
+word = input('Enter the word : ').lower()
+
+print(dic1[word])
