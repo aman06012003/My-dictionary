4a8949170fc77c7f2c8baf0e135f459860ed2483
Aman Kapoor <akalasklo@gmail.com>
2021-06-12 20:41:16 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,6 +1,6 @@
 print('Welcome to my dictionary')
 
-dic1 = {'abacus':'A frame of wire with rowsof wires along which beads are slid', 'abandon': 'give up'}
+dic1 = {'abacus':'A frame of wire with rowsof wires along which beads are slid', 'abandon': 'give up','abase': 'humiliate or degrade', 'abashed': 'embarrassed'}
 
 word = input('Enter the word : ').lower()
 
