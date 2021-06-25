728aeaf8af55dd32b71476d3ff2b425737e133f4
Aman Kapoor <akalasklo@gmail.com>
2021-06-25 12:07:09 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,4 @@
-import webbrowsers
+import webbrowser
 print('Welcome to my dictionary')
 
 dic1 = {'abacus':'A frame of wire with rowsof wires along which beads are slid', 'abandon': 'give up','abase': 'humiliate or degrade', 'abashed': 'embarrassed','book':'a written or printed work consisting of pages bound in a cover','bread':'a food made of flour','brilliant':'very clever or talented'}
