1b2103013949ac01af4dd8cdf1bb582b7dcab16e
Aman Kapoor <akalasklo@gmail.com>
2021-06-25 12:05:38 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,7 +1,7 @@
 import webbrowsers
 print('Welcome to my dictionary')
 
-dic1 = {'abacus':'A frame of wire with rowsof wires along which beads are slid', 'abandon': 'give up','abase': 'humiliate or degrade', 'abashed': 'embarrassed'}
+dic1 = {'abacus':'A frame of wire with rowsof wires along which beads are slid', 'abandon': 'give up','abase': 'humiliate or degrade', 'abashed': 'embarrassed','book':'a written or printed work consisting of pages bound in a cover','bread':'a food made of flour','brilliant':'very clever or talented'}
 
 try:
   word = input('Enter the word : ').lower()
